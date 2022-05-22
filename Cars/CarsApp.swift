d17c3528959200354571dcf43dd7281cae21b134
Mohamed Abd-Elhamed Ahmed <73020992+Mohamedabdelhamed382@users.noreply.github.com>
2022-05-23 00:14:06 +0200

    Initial Commit

---
+++ Cars/CarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsApp.swift
+//  Cars
+//
+//  Created by MOHAMED ABD ELHAMED AHMED on 23/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
